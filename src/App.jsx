116f279e3389bb7f6ec1b25f1e548fa548f727b1
liaojiajia <786602661@qq.com>
2021-07-23 16:07:30 +0800

    react start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+// import './App.less';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Button type="primary">Button</Button>
+            </div>
+        )
+    }
+}
